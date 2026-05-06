df791b46dae5e53c5897cb134eba230e85d97937
THAYANG <deponselb@gmail.com>
2026-05-07 04:43:11 +0900

    Update MikrotikService.php

--- app/Services/Services/MikrotikService.php
+++ app/Services/Services/MikrotikService.php
@@ -1,1 +1,19 @@
 
+public function generateVoucher($code, $profile, $limitUptime, $comment)
+{
+    if (!$this->client) return false;
+
+    $request = new RouterOS\Request('/ip/hotspot/user/add');
+    $request->setArgument('name', $code);
+    $request->setArgument('password', $code); // Password disamakan dengan kode
+    $request->setArgument('profile', $profile);
+    $request->setArgument('limit-uptime', $limitUptime);
+    $request->setArgument('comment', $comment);
+
+    try {
+        $this->client->sendSync($request);
+        return true;
+    } catch (\Exception $e) {
+        return false;
+    }
+}
